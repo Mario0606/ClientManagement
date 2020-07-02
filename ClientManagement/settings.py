0087c4282859f087afd67bffabb5edfc6b159c1f
Mario <mario.nascimento@aluno.uece.br>
2020-07-01 23:55:20 -0300

    Add livereload

--- ClientManagement/settings.py
+++ ClientManagement/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrapform',
+    'livereload',
     'clients',
     'home'
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript'
 ]
 
 ROOT_URLCONF = 'ClientManagement.urls'
